b34416f7a7464008f43480f09e622972b3485374
Gautham P <gauthampdn@gmail.com>
2023-08-05 09:42:25 -0700

    added cors

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,13 @@ const express = require("express")
 const mongoose = require("mongoose")
 const workoutRoutes = require("./routes/workouts")
 const userRoutes = require("./routes/user")
-
+const cors = require('cors');
 
 // express app
 const app = express()
+
+app.use(cors());
+
 
 // middleware
 app.use(express.json()) // to get req body
