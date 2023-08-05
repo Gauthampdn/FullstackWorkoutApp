968f8af858f7446da643cde02bfc3feb236c0ab6
Gautham P <gauthampdn@gmail.com>
2023-08-05 10:18:23 -0700

    changed relative path

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts', {
+      const response = await fetch('https://mernworkout.onrender.com/api/workouts', {
         headers: {
           "Authorization": `Bearer ${user.token}`    // backticks for template string to have variable inside
         }
